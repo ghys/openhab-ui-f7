1a1a94223467ca66d3a1ff26f782d23d3b657962
YS <ghys@users.noreply.github.com>
2019-04-26 13:07:49 +0200

    remove Quasar commands for Cypress

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -25,65 +25,10 @@ Cypress.Commands.add('restoreLocalStorage', () => {
   })
 })
 
-// CHAINABLE QUASAR INPUT FIELD TYPES
-// usage:
-//
-// cy.get('[data-cy=target-element]').quasar('text', '');
-//
-// todo: make sure that this is still compliant with 1.0
-//
 Cypress.Commands.add('testRoute', (route) => {
     cy.location().should((loc) => {
       expect(loc.hash).to.contain(route)
     })
-})
-
-Cypress.Commands.add('quasar', { prevSubject: 'element' }, (subject, mode, option) => {
-  if (mode === 'select') {
-    cy.wrap(subject)
-      .invoke('show')
-      .click({ force: true })
-      .then(() => {
-        cy.get('.q-popover')
-          .contains(option)
-          .click()
-      })
-  } else if (mode === 'grid') {
-    cy.wrap(subject).within(() => {
-      cy.get('input').click({ force: true, multiple: true });
-    })
-  } else if (mode === 'tag-list') {
-    Object.keys(option)
-    .forEach((x) => {
-      cy.wrap(subject)
-        .within(() => {
-          cy.get('input')
-            .first()
-            .type(`${option[x]}{enter}`)
-      })
-    })
-  } else {
-    cy.wrap(subject)
-      .invoke('show')
-      .within(($subject) => { // eslint-disable-line
-        switch (mode) {
-          case 'date':
-          case 'text':
-          case 'email':
-            cy.get('input:first')
-              .type(option)
-              .should('have.value', option)
-            break;
-          case 'radio':
-          case 'checkbox':
-            cy.contains(option)
-              .click()
-            break
-          default:
-            break
-        }
-    })
-  }
 })
 
 // Cypress.Commands.add('loadStore', () => {});
