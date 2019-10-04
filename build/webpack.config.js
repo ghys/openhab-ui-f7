222f1450ddb5135b8014491264dbc78a1475ef53
Yannick Schaus <github@schaus.net>
2019-10-04 21:06:15 +0200

    Add experimental nearley parser for items files

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,44 +1,43 @@
-const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
-const WebpackAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
+const WebpackAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
-const path = require('path');
+const path = require('path')
 
-function resolvePath(dir) {
-  return path.join(__dirname, '..', dir);
+function resolvePath (dir) {
+  return path.join(__dirname, '..', dir)
 }
 
-const env = process.env.NODE_ENV || 'development';
-const target = process.env.TARGET || 'web';
-const buildSourceMaps = process.env.SOURCE_MAPS || false;
-const isCordova =  target === 'cordova';
+const env = process.env.NODE_ENV || 'development'
+const target = process.env.TARGET || 'web'
+const buildSourceMaps = process.env.SOURCE_MAPS || false
+const isCordova = target === 'cordova'
 
 // const apiBaseUrl = 'http://openhab:8080' // 'http://demo.openhab.org:8080' // 'http://localhost:8080'
 const apiBaseUrl = 'http://localhost:8080'
 
-
 module.exports = {
   mode: env,
   entry: [
-    './src/js/app.js',
+    './src/js/app.js'
   ],
   output: {
     path: resolvePath(isCordova ? 'cordova/www' : 'www'),
     filename: 'js/app.js',
-    publicPath: '',
+    publicPath: ''
   },
   resolve: {
     extensions: ['.mjs', '.js', '.vue', '.json'],
     alias: {
       vue$: 'vue/dist/vue.esm.js',
-      '@': resolvePath('src'),
-    },
+      '@': resolvePath('src')
+    }
   },
   devtool: env === 'production' ? (buildSourceMaps) ? 'source-map' : 'none' : 'eval-source-map',
   devServer: {
@@ -78,12 +77,12 @@ module.exports = {
           resolvePath('node_modules/vue-echarts'),
           resolvePath('node_modules/resize-detector'),
           resolvePath('node_modules/later-again')
-        ],
+        ]
       },
 
       {
         test: /\.vue$/,
-        use: 'vue-loader',
+        use: 'vue-loader'
       },
       {
         test: /\.css$/,
@@ -95,8 +94,8 @@ module.exports = {
             }
           }),
           'css-loader',
-          'postcss-loader',
-        ],
+          'postcss-loader'
+        ]
       },
       {
         test: /\.styl(us)?$/,
@@ -109,8 +108,8 @@ module.exports = {
           }),
           'css-loader',
           'postcss-loader',
-          'stylus-loader',
-        ],
+          'stylus-loader'
+        ]
       },
       {
         test: /\.less$/,
@@ -123,8 +122,8 @@ module.exports = {
           }),
           'css-loader',
           'postcss-loader',
-          'less-loader',
-        ],
+          'less-loader'
+        ]
       },
       {
         test: /\.(sa|sc)ss$/,
@@ -137,39 +136,45 @@ module.exports = {
           }),
           'css-loader',
           'postcss-loader',
-          'sass-loader',
-        ],
+          'sass-loader'
+        ]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: 'images/[name].[ext]',
-        },
+          name: 'images/[name].[ext]'
+        }
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: 'media/[name].[ext]',
-        },
+          name: 'media/[name].[ext]'
+        }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: 'fonts/[name].[ext]',
-        },
+          name: 'fonts/[name].[ext]'
+        }
       },
-    ],
+      {
+        test: /\.nearley$/,
+        use: [
+          'nearley-loader'
+        ]
+      }
+    ]
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(env),
-      'process.env.TARGET': JSON.stringify(target),
+      'process.env.TARGET': JSON.stringify(target)
     }),
     new VueLoaderPlugin(),
     ...(env === 'production' ? [
@@ -182,19 +187,19 @@ module.exports = {
         //   },
         // },
         sourceMap: true,
-        parallel: true,
+        parallel: true
       }),
       new OptimizeCSSPlugin({
         cssProcessorOptions: {
           safe: true,
-          map: { inline: false },
-        },
+          map: { inline: false }
+        }
       }),
-      new webpack.optimize.ModuleConcatenationPlugin(),
+      new webpack.optimize.ModuleConcatenationPlugin()
     ] : [
       // Development only plugins
       new webpack.HotModuleReplacementPlugin(),
-      new webpack.NamedModulesPlugin(),
+      new webpack.NamedModulesPlugin()
     ]),
     new HtmlWebpackPlugin({
       filename: './index.html',
@@ -207,29 +212,29 @@ module.exports = {
         removeScriptTypeAttributes: true,
         removeStyleLinkTypeAttributes: true,
         useShortDoctype: true
-      } : false,
+      } : false
     }),
     new MiniCssExtractPlugin({
-      filename: 'css/app.css',
+      filename: 'css/app.css'
     }),
     new CopyWebpackPlugin([
       {
         from: resolvePath('src/static'),
-        to: resolvePath(isCordova ? 'cordova/www/static' : 'www/static'),
+        to: resolvePath(isCordova ? 'cordova/www/static' : 'www/static')
       },
       {
         from: resolvePath('src/manifest.json'),
-        to: resolvePath('www/manifest.json'),
-      },
+        to: resolvePath('www/manifest.json')
+      }
     ]),
     ...(!isCordova ? [
       new WorkboxPlugin.InjectManifest({
-        swSrc: resolvePath('src/service-worker.js'),
+        swSrc: resolvePath('src/service-worker.js')
       })
-    ] : []),
+    ] : [])
     // new WebpackAnalyzerPlugin({
     //   analyzerMode: 'static',
     //   reportFilename: '../report.html'
     // })
-  ],
-};
+  ]
+}
--- src/pages/developer/developer-tools.vue
+++ src/pages/developer/developer-tools.vue
@@ -1,50 +1,120 @@
 <template>
   <f7-page name="devtools" @page:beforeremove="onPageBeforeRemove">
-    <f7-navbar title="Developer Tools" back-link="Back" back-link-url="/" back-link-force></f7-navbar>
-    <f7-block-title class="after-big-title">Test SSE connection</f7-block-title>
-    <f7-block strong>
-      <p>Start a SSE connection to check the different implementations</p>
-      <f7-button text="Stream Events" @click="startSSE()" v-if="!sseClient" />
-      <f7-button text="Stop Streaming" @click="stopSSE()" v-if="sseClient" />
-    </f7-block>
-    <f7-list media-list>
-      <f7-list-item v-for="event in sseEvents" :key="event.time.getTime()" :title="event.topic" :subtitle="event.payload" :after="event.type">
-      </f7-list-item>
-    </f7-list>
-    <f7-block strong>
-      <p>Test an icon fetch to check the different implementations</p>
-      <f7-list media-list>
-        <f7-list-input
-          label="Icon"
-          type="text"
-          :value="icon"
-          @change="icon = $event.target.value"
-          placeholder="e.g. lightbulb, qualityofservice-2"
-          :info="iconUrl"
-          clear-button
-        >
-          <img :src="iconUrl" width="44" slot="media" />
-        </f7-list-input>
-      </f7-list>
-    </f7-block>
-    <f7-block>
-      <cron-editor />
-    </f7-block>
+    <f7-navbar title="Developer Tools" back-link="Back" back-link-url="/" back-link-force>
+    </f7-navbar>
+    <f7-toolbar tabbar position="top">
+      <f7-link @click="currentTab = 'sse'" :tab-link-active="currentTab === 'sse'" class="tab-link">SSE &amp; Icons</f7-link>
+      <f7-link @click="currentTab = 'parser'" :tab-link-active="currentTab === 'parser'" class="tab-link">Items file parser</f7-link>
+      <f7-nav-right>
+        <!-- <f7-link
+          class="searchbar-enable"
+          data-searchbar=".searchbar-demo"
+          icon-ios="f7:search_strong"
+          icon-aurora="f7:search_strong"
+          icon-md="material:search"
+        ></f7-link> -->
+      </f7-nav-right>
+    </f7-toolbar>
+
+    <f7-tabs>
+      <f7-tab id="info" @tab:show="() => this.currentTab = 'sse'" :tab-active="currentTab === 'sse'">
+        <f7-block class="block-narrow">
+          <f7-row>
+            <f7-col>
+              <f7-block>
+                <f7-block-title class="after-big-title">Test SSE connection</f7-block-title>
+                <p>Start a SSE connection to check the different implementations</p>
+                <f7-button text="Stream Events" @click="startSSE()" v-if="!sseClient" />
+                <f7-button text="Stop Streaming" @click="stopSSE()" v-if="sseClient" />
+                <f7-list media-list>
+                  <f7-list-item v-for="event in sseEvents" :key="event.time.getTime()" :title="event.topic" :subtitle="event.payload" :after="event.type">
+                  </f7-list-item>
+                </f7-list>
+              </f7-block>
+            </f7-col>
+          </f7-row>
+        </f7-block>
+        <f7-block class="block-narrow">
+          <f7-row>
+            <f7-col>
+              <f7-block>
+                <p>Test an icon fetch to check the different implementations</p>
+                <f7-list media-list>
+                  <f7-list-input
+                    label="Icon"
+                    type="text"
+                    :value="icon"
+                    @change="icon = $event.target.value"
+                    placeholder="e.g. lightbulb, qualityofservice-2"
+                    :info="iconUrl"
+                    clear-button
+                  >
+                    <img :src="iconUrl" width="44" slot="media" />
+                  </f7-list-input>
+                </f7-list>
+              </f7-block>
+            </f7-col>
+          </f7-row>
+        </f7-block>
+      </f7-tab>
+      <f7-tab id="parser" @tab:show="() => this.currentTab = 'parser'" :tab-active="currentTab === 'parser'">
+        <!-- <f7-block>
+          <f7-row>
+            <f7-col>
+              <f7-button>Parse</f7-button>
+            </f7-col>
+          </f7-row>
+        </f7-block> -->
+        <div class="row">
+          <div class="col">
+            <editor class="items-parser" :value="itemsDsl" @input="(value) => itemsDsl = value" />
+            <pre class="items-results"><code>{{parsedItems}}</code></pre>
+            <!-- <editor class="items-results" :value="parsedItems" /> -->
+          </div>
+          <!-- <div class="col parse-results">
+            <editor class="items-parser" :value="itemsDsl" @input="(value) => itemsDsl = value" />
+          </div> -->
+        </div>
+      </f7-tab>
+    </f7-tabs>
   </f7-page>
 </template>
 
+<style lang="stylus">
+.items-parser.vue-codemirror
+  display block
+  top calc(var(--f7-navbar-height) + var(--f7-toolbar-height))
+  height calc(50% - var(--f7-navbar-height) - var(--f7-toolbar-height))
+  width 100%
+.items-results
+  position absolute
+  top 50%
+  padding 0 1rem
+.parse-results
+  position relative
+  left 0
+  width 100%
+  pre
+    font-size 12px
+    height calc(100% - var(--f7-navbar-height) + var(--f7-toolbar-height) - 100px)
+    overflow auto
+</style>
+
 <script>
-import CronEditor from '@/components/config/controls/cronexpression-editor.vue'
+import { Parser, Grammar } from 'nearley'
+import grammar from '@/assets/items.nearley'
 
 export default {
   components: {
-    CronEditor
+    'editor': () => import('@/components/config/controls/script-editor.vue')
   },
   data () {
     return {
+      currentTab: 'sse',
       sseClient: null,
       sseEvents: [],
-      icon: 'lightbulb'
+      icon: 'lightbulb',
+      itemsDsl: ''
     }
   },
   methods: {
@@ -62,6 +132,19 @@ export default {
       this.$oh.sse.close(this.sseClient)
       this.sseClient = null
       this.sseEvents = []
+    }
+  },
+  computed: {
+    parsedItems () {
+      try {
+        const parser = new Parser(Grammar.fromCompiled(grammar))
+        parser.feed(this.itemsDsl.trim())
+        if (!parser.results.length) return 'Unable to parse, check your input'
+        // return parser.results[0].map((i) => i.name).join('\n')
+        return JSON.stringify(parser.results[0].filter((i) => i !== null), null, 4)
+      } catch (e) {
+        return e
+      }
     }
   },
   asyncComputed: {
